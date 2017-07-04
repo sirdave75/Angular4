441ca0f8825ce34213627ba6cfaa576cdb98b29f
David Romanillos <david.romanillos@sildaviaweb.com>
2017-07-04 14:17:36 +0200

    tipos de  variables, ngOnInit y constructor

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -11,20 +11,53 @@ export class FrutaComponent{
       public nombre_componente = 'Componente de fruta';
       public listado_frutas = 'Naranja, Manzana, Pera, Sandía';
 
-      public nombre:string = 'David';
+      public nombre:string;
 
-      public edad:number = 42;
+      public edad:number;
 
-      public mayorDeEdad:boolean = true;
+      public mayorDeEdad:boolean;
 
       public trabajos:Array<any> = ['Carpintero','Programador','Reponedor',52];
 
-      comodin:any = 'Cualquier cosa';
+      comodin:any;
 
 
       constructor(){
+            this.nombre = 'David Romanillos';
+            this.edad = 42;
+            this.mayorDeEdad = true;
+            this.comodin = 'SI';
 
-            console.log(this.trabajos);
+      }
+
+
+      ngOnInit(){
+
+          this.cambiarNombre();
+          this.cambiarEdad(45);
+          console.log(this.nombre + " - " + this.edad);
+
+
+      //VAriables y alcance
+        var uno = 8;
+        var dos = 15;
+        if(uno === 8) {
+            let uno = 3; //actua dentro del if (del bloque)
+            var dos = 88; // actúa de forma global
+
+
+            console.log ('Dentro del if ' + uno + " " +dos);
+        }
+          console.log ('Fuera del if ' + uno + " " + dos);
+      }
+
+
+
+      cambiarNombre(){
+          this.nombre = 'Pedro';
+      }
+      cambiarEdad(edad){
+          this.edad = edad;
       }
 
 }

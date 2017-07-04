f14e12f7e0f22eeab7b4215161c73f9927a920d5
David Romanillos <david.romanillos@sildaviaweb.com>
2017-07-04 14:45:33 +0200

    crear una clase, instanciarla

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -2,12 +2,28 @@
  * Created by binll on 04/07/2017.
  */
 import { Component } from '@angular/core';
-
+import { Empleado } from './empleado';
 @Component({
     selector: 'empleado',
     templateUrl: './empleado.component.html'
 })
 
 export class EmpleadoComponent{
-    public titulo = 'Esto es un component empleado';
+    public titulo = 'Componente Empleados';
+    public empleado:Empleado;
+    public trabajadores:Array<Empleado>;
+
+    constructor(){
+        this.empleado = new Empleado('David Romanillos',42,'Programador',true);
+        this.trabajadores = [
+            new Empleado('David Romanillos',42,'Programador',true);
+            new Empleado('Sergio Mestre',41,'Programador',true);
+            new Empleado('Angel Luis Lasierra',52,'Gerente',true);
+        ];
+    }
+
+    ngOnInit(){
+        console.log(this.trabajadores);
+
+    }
 }

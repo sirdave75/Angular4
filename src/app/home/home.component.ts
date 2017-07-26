7c0f0ecd0f909c4483089022bb8e112d078d66ff
David Romanillos <david.romanillos@sildaviaweb.com>
2017-07-26 14:04:42 +0200

    Servicios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { RopaService } from '../services/ropa.service';
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    providers : [RopaService]
 })
 
 export  class HomeComponent{
+
     public  titulo = 'Página principal';
+    public listado_ropa: Array<string>;
+    public prenda_a_guardar: string;
+
+    constructor(
+        private _ropaService: RopaService
+    ){}
+
+    ngOnInit(){
+            this.listado_ropa = this._ropaService.getRopa();
+            console.log(this.listado_ropa);
+    }
+
+    guardarPrenda(){
+        this._ropaService.addRopa(this.prenda_a_guardar);
+        this.prenda_a_guardar = null;
+    }
+
+    eliminarPrenda(index:number){
+        this._ropaService.deleteRopa(index);
+        alert(index);
+    }
 }
---
+++ src/app/services/ropa.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RopaService{
+    public nombre_prenda = 'Pantalones vaqueros';
+
+    public coleccion_ropa = ['Pantalones blancos', 'Camiseta roja'];
+
+    prueba(nombre_prenda){
+        return nombre_prenda;
+    }
+
+    addRopa(nombre_prenda:string):Array<string>{
+        this.coleccion_ropa.push(nombre_prenda);
+        return this.getRopa();
+    }
+    getRopa():Array<string>{
+        return this.coleccion_ropa;
+    }
+
+    deleteRopa(index:number){
+        this.coleccion_ropa.splice(index,1);
+
+        return this.getRopa();
+    }
+
+}
